5f8199925a2cea24b58bc39b9f69e2e9534c7cd3
phylyc <0d26ae7a@opayq.com>
2025-08-08 08:57:37 -0400

    better fix for getting mutation histograms

--- BuildTree/CellPopulation.py
+++ BuildTree/CellPopulation.py
@@ -101,10 +101,13 @@ def load_clustering_results(cluster_info_file, patient_data):
         cluster.set_blacklist_status()
         clustering_results[cluster_id] = cluster
 
-    # Add mutations to the cluster
-    mutations = patient_data.sample_list[0].mutations
-    for mutation in mutations:
-        cluster_id = mutation.cluster_assignment
-        clustering_results[cluster_id].add_mutation(mutation, mutation.ccf_1d)
+    # Create for each cluster dictionary of mutations (key - mut_var_str and value- nd_histogram in log space)
+    mutations_nd_hist = {}
+    for mutation in patient_data.sample_list[0].mutations:
+        if mutation not in mutations_nd_hist:
+            mutations_nd_hist[mutation] = []
+        mutations_nd_hist[mutation].append(mutation.ccf_1d)
+    for mutation, mutation_nd_hist in mutations_nd_hist.items():
+        clustering_results[mutation.cluster_assignment].add_mutation(mutation, mutation_nd_hist)
 
     patient_data.ClusteringResults = clustering_results
